95ac4b7bc5e616a8fc1f7d314c9f710ceb336612
9629195960 <31506027+9629195960@users.noreply.github.com>
2018-02-05 16:20:28 +0530

    Create addd.C

---
+++ addd.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,n,k,sum=0,count=0;
+	scanf("%d",&n);
+	scanf("%d",&k);
+	for(i=0;i<=k;i++)
+	{
+		sum=sum+i;
+		count++;
+	}
+	printf("%d",sum);
+	return 0;
+}
