5d89c2e047876ad0676726abe2f3d89313b8ed3d
9629195960 <31506027+9629195960@users.noreply.github.com>
2018-03-14 16:18:14 +0530

    Create a79.C

---
+++ a79.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,i,n;
+	scanf("%d",&n);
+	scanf("%d",&a);
+	i=n*a;
+	if(a==n)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+
+	return 0;
+}
